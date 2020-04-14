2c946e0b3faf795e358e77129f6f3329e7e93382
xn2001 <jialna@qq.com>
2020-04-14 20:44:08 +0800

    Eureka集群架构

--- cloud-provider-payment8001/src/main/java/com/xn2001/springcloud/service/PaymentService.java
+++ cloud-provider-payment8001/src/main/java/com/xn2001/springcloud/service/PaymentService.java
@@ -1,7 +1,6 @@
 package com.xn2001.springcloud.service;
 
 import com.xn2001.springcloud.entities.Payment;
-import org.apache.ibatis.annotations.Param;
 
 /**
  * Created by 乐心湖 on 2020/3/31 22:40
---
+++ cloud-provider-payment8002/src/main/java/com/xn2001/springcloud/PaymentMain8002.java
@@ -0,0 +1,7 @@
+package com.xn2001.springcloud;
+
+/**
+ * Created by 乐心湖 on 2020/4/14 20:43
+ */
+public class PaymentMain8002 {
+}
